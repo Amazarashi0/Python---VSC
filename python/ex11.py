97eaa5d534ce741352e2bec68b1d2145de408aaa
0vers <2803175913s@gmail.com>
2021-10-25 16:34:52 +0800

    master

--- python/ex11.py
+++ python/ex11.py
@@ -1,2 +1,21 @@
-print("How old are you", end = "")
+print("How old are you?", end = "")
 age = input()
+print("How tall are you?", end = "")
+height = input()
+print("Howmuch do you weight?", end = "")
+weight = input()
+
+print(f"So, you are {age} old, {height} tall and {weight} heavy.\n\n")
+
+print("# the other example:\n")
+print("\\*\fwhat is x\f?", end = "")
+x = int(input("{}x = ".format("*" * 6)))
+print("\\*\twhat is y ?", end = "")
+y = int(input("*y = "))
+
+print("#\t\bz = x + y =", x + y)
+
+# 提问并输入答案
+# 1）接收输入的内容
+# 2）改变输入的内容
+# 3）打印出输入的内容
