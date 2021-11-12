276cd6334b2785f715b1ff0607a744669961bb6e
0vers <2803175913s@gmail.com>
2021-11-12 19:48:15 +0800

    master

--- python/ex13.py
+++ python/ex13.py
@@ -1,8 +1,17 @@
 from sys import argv
 # read the WYSS section for how to run this
 sciript, first, second = argv
-
 print("the script is callde:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+
+
+#1：将sys模块导入脚本(script)（即你写的python程序）
+# import(导入)语句——将python的模组（模块module，也称作库library）引入脚本
+# argv——参数变量(argument variable):保存着运行python脚本时传递给脚本的参数。
+
+#第3行——将argv解包：将argv中的内容取出，解包，将所有的参数依次赋值给左边的变量。
+
+### 一旦程序用到argv，要注意调用程序时必须添加完整的命令行参数。
+# 例如该程序需要传递3个命令行参数：python ex13.py first 2nd 3rd
