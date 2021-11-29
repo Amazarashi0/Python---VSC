f8ea3936bf5f008f5d481ccf3b01bec01d99c24a
0vers <2803175913s@gmail.com>
2021-11-29 16:02:52 +0800

    master

--- python/ex11.py
+++ python/ex11.py
@@ -17,7 +17,23 @@ y = int(input("*y = "))
 
 print("#\t\bz = x + y =", x + y)
 
-# 提问并输入答案
+## 提问并输入答案
 # 1）接收输入的内容
 # 2）改变输入的内容
 # 3）打印出输入的内容
+
+## end = ""用来提示print不用换行符结束这一行
+
+# # # Help on built-in function input in module builtins:
+#
+# input(prompt=None, /)
+#     Read a string from standard input.  The trailing newline is stripped.
+#
+#     The prompt string, if given, is printed to standard output without a
+#     trailing newline before reading input.
+#
+#     If the user hits EOF (*nix: Ctrl-D, Windows: Ctrl-Z+Return), raise EOFError.
+#     On *nix systems, readline is used if available.
+
+#    从标准输入中读取字符串，并取消换行尾符。
+#    如果给出了提示字符串，则在读取输入前将不带换行尾符的提示字符串打印到标准输出。
--- python/ex4.py
+++ python/ex4.py
@@ -23,3 +23,14 @@ print ("three are", 100, "cars available.")
 
 # “=”：为变量赋值。将右边的值赋给左边的变量名
 # "=="：检查左右两边的值是否相等
+
+# # # Help on built-in function print in module builtins:
+# print(...)
+#    print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+#
+#    Prints the values to a stream, or to sys.stdout by default.
+#    Optional keyword arguments:
+#    file:  a file-like object (stream); defaults to the current sys.stdout.
+#    sep:   string inserted between values, default a space.
+#    end:   string appended after the last value, default a newline.
+#    flush: whether to forcibly flush the stream.
